e721a014b95b1c3cc53ab4ad8ed7a0c5230aa375
SameerHRV <sameerharapanahalli2004@gmail.com>
2024-05-27 18:11:03 +0530

    add new group

--- frontend/src/components/shared/UserItem.jsx
+++ frontend/src/components/shared/UserItem.jsx
@@ -1,8 +1,8 @@
-import { Add as AddIcon } from "@mui/icons-material";
+import { Add as AddIcon, Remove as RemoveIcon } from "@mui/icons-material";
 import { Avatar, IconButton, ListItem, Stack, Typography } from "@mui/material";
 import React, { memo } from "react";
 
-const UserItem = ({ user, handler, handlerIsLoading }) => {
+const UserItem = ({ user, handler, handlerIsLoading, isAdded = false }) => {
   const { name, _id, avatar } = user;
   return (
     <ListItem>
@@ -24,16 +24,17 @@ const UserItem = ({ user, handler, handlerIsLoading }) => {
         <IconButton
           size="small"
           sx={{
-            bgcolor: "primary.main",
+            bgcolor: isAdded ? "error.main" : "primary.main",
             color: "white",
             "&:hover": {
-              bgcolor: "primary.dark",
+              bgcolor: isAdded ? "error.dark" : "primary.dark",
             },
           }}
           onClick={() => handler(_id)}
           disabled={handlerIsLoading}
         >
-          <AddIcon />
+          {isAdded ? <RemoveIcon /> : <AddIcon />}
+          {/* <AddIcon /> */}
         </IconButton>
       </Stack>
     </ListItem>
--- frontend/src/components/specific/NewGroup.jsx
+++ frontend/src/components/specific/NewGroup.jsx
@@ -1,24 +1,52 @@
-import { Dialog, DialogTitle, Stack, Typography } from "@mui/material";
-import React from "react";
+import { Button, Dialog, DialogTitle, Stack, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { sampleUsers } from "../../constants/sampleData";
+import UserItem from "../shared/UserItem";
+import { useInputValidation } from "6pp";
 
 const NewGroup = () => {
+  const groupname = useInputValidation("");
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectedMenber, setselectedMember] = useState([]);
+
+  const selectMemberHandler = (id) => {
+    setselectedMember((prev) => (prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id]));
+  };
+  console.log(setselectedMember);
+
+  const submitHandler = () => {};
+
+  const closeHandler = () => {};
   return (
     <Dialog open>
       <Stack
         padding={{
           xs: "1rem",
-          sm: "2rem",
+          sm: "3rem",
         }}
-        maxWidth={"25rem"}
+        width={"25rem"}
+        spacing={"2rem"}
       >
-        <DialogTitle>Notification</DialogTitle>
-        {sampleNotifications.length > 0 ? (
-          sampleNotifications.map((i) => (
-            <NotificationItem sender={i.sender} _id={i._id} handler={frendRequestHandler} key={i._id} />
-          ))
-        ) : (
-          <Typography textAlign={"center"}>0 Notification</Typography>
-        )}
+        <DialogTitle textAlign={"center"} variant="h4">
+          New group
+        </DialogTitle>
+
+        <TextField label="Group Name" value={groupname.value} onChange={groupname.changeHandler} />
+        <Typography variant="body1">Members</Typography>
+
+        <Stack>
+          {members.map((i) => (
+            <UserItem user={i} key={i._id} handler={selectMemberHandler} isAdded={selectedMenber.includes(i._id)} />
+          ))}
+        </Stack>
+        <Stack direction={"row"} justifyContent={"space-evenly"}>
+          <Button variant="outlined" color="error" size="large">
+            Cancal
+          </Button>
+          <Button variant="contained" size="large" onClick={submitHandler}>
+            Create
+          </Button>
+        </Stack>
       </Stack>
     </Dialog>
   );
